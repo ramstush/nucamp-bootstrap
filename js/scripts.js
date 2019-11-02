19ebdf19a40449c09c1bc5f23e67febb0191aedc
Ryan Stush <ramstush@gmail.com>
2019-11-02 12:58:49 -0400

    converted html.index to use jquery for modals instead of bootstrap, updated scss and script js accordingly

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,37 @@
-$(function () {
+$(function() {
     $(".carousel").carousel({
-        interval: 2000
+      interval: 2000
     });
-    $("#carouselButton").click(function () {
-        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
-            $(".carousel").carousel("pause");
-            $("#carouselButton").children("i").removeClass("fa-pause");
-            $("#carouselButton").children("i").addClass("fa-play");
-        } else {
-            $(".carousel").carousel("cycle");
-            $("#carouselButton").children("i").removeClass("fa-play");
-            $("#carouselButton").children("i").addClass("fa-pause");
-        }
+    $("#carouselButton").click(function() {
+      if (
+        $("#carouselButton")
+          .children("i")
+          .hasClass("fa-pause")
+      ) {
+        $(".carousel").carousel("pause");
+        $("#carouselButton")
+          .children("i")
+          .removeClass("fa-pause");
+        $("#carouselButton")
+          .children("i")
+          .addClass("fa-play");
+      } else {
+        $(".carousel").carousel("cycle");
+        $("#carouselButton")
+          .children("i")
+          .removeClass("fa-play");
+        $("#carouselButton")
+          .children("i")
+          .addClass("fa-pause");
+      }
     });
-});
+  
+    $("#reserveButton").click(function() {
+      $("#reserveModal").modal("show");
+    });
+  
+    $("#loginButton").click(function() {
+      $("#loginModal").modal("show");
+    });
+  });
+  
